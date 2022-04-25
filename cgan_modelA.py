87e8c8bb6ad1ddd836b50b7ca6f10179f9f6223c
TMarquet <72377479+TMarquet@users.noreply.github.com>
2022-04-25 10:13:12 +0200

    Update cgan_modelA.py

--- cgan_modelA.py
+++ cgan_modelA.py
@@ -113,7 +113,7 @@ def discriminator_loss(real_output, fake_output):
 def discriminator_accuracy(real_output, fake_output):
     real_accuracy = tf.reduce_sum(tf.where(real_output >= 0.5, tf.ones_like(real_output), tf.zeros_like(real_output)))
     fake_accuracy = tf.reduce_sum(tf.where(fake_output >= 0.5, tf.zeros_like(fake_output), tf.ones_like(fake_output)))
-    return fake_accuracy, real_accuracy
+    return fake_accuracy / (fake_output.shape[0]), real_accuracy/ (real_output.shape[0])
 
 
 def generator_loss(fake_output):
