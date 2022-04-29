d1c25b1ceffeec69f341e33d01435f7ed334903c
TMarquet <72377479+TMarquet@users.noreply.github.com>
2022-04-29 10:19:45 +0200

    Update trace_generation.py

--- trace_generation.py
+++ trace_generation.py
@@ -31,6 +31,6 @@ if __name__ == "__main__":
         images = image if images is None else np.append(images,image,axis =0)
         labels = label if labels is None else np.append(labels ,label ,axis =0)
     np.save('images.npy',images,allow_pickle= True ) 
-    np.save('labels.npy',label,allow_pickle= True)
+    np.save('labels.npy',labels,allow_pickle= True)
 
 
