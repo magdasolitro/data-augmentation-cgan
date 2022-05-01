9efe70723d14bc342da6ab972bd43ce795bae559
Magdalena <magda.solitro@icloud.com>
2022-05-01 10:52:32 +0200

    included snr script

--- cgan_modelA.py
+++ cgan_modelA.py
@@ -50,21 +50,17 @@ for intermediate in INTERMEDIATES:
               17 if int(intermediate[len(list(intermediate)) - 1]) == 1 else 33)]
 
 
-def make_generator_model(n_classes = 256,embedding_dim = 100):
-    
-    
-    noise_input = layers.Input(shape = (100,))
+def make_generator_model(n_classes=256, embedding_dim=100):
+    noise_input = layers.Input(shape=(100,))
 
-
-
-    reshaped = layers.Reshape((1,100))(noise_input)
-    label_input = layers.Input(shape = (1,))
+    reshaped = layers.Reshape((1, 100))(noise_input)
+    label_input = layers.Input(shape=(1,))
     embedded = layers.Embedding(n_classes, embedding_dim)(label_input)
 
     concat_input = layers.Concatenate()([reshaped, embedded])
   
     x = layers.Dense(500)(concat_input)
-    x = layers.Reshape((500,1))(x)
+    x = layers.Reshape((500, 1))(x)
     x = layers.Conv1DTranspose(500, 5, strides=1, padding='same', use_bias=False)(x)
     x = layers.BatchNormalization()(x)
     x = layers.LeakyReLU()(x)
@@ -90,14 +86,12 @@ def make_generator_model(n_classes = 256,embedding_dim = 100):
 
 
 def make_discriminator_model(n_classes=256, embedding_dim=8):
-
-    image_input = layers.Input(shape=(50,20))
+    image_input = layers.Input(shape=(50, 20))
     label_input = layers.Input(shape=(1,))
     embedded = layers.Embedding(n_classes, embedding_dim)(label_input)
     dense = layers.Dense(1000)(embedded)
     reshaped = layers.Reshape((50, 20))(dense)
     concat_input = layers.Concatenate()([image_input, reshaped])
-    
 
     x = layers.Conv1D(32, 5, strides=2, padding='same')(concat_input)
     x = layers.Lambda(lambda x: K.l2_normalize(x,axis=1))(x)
@@ -105,7 +99,7 @@ def make_discriminator_model(n_classes=256, embedding_dim=8):
     x = layers.LeakyReLU()(x)
     
     x = layers.Conv1D(64, 5, strides=2, padding='same')(x)
-    x = layers.Lambda(lambda x: K.l2_normalize(x,axis=1))(x)
+    x = layers.Lambda(lambda x: K.l2_normalize(x, axis=1))(x)
     x = layers.BatchNormalization()(x)
     x = layers.LeakyReLU()(x)
 
@@ -114,7 +108,6 @@ def make_discriminator_model(n_classes=256, embedding_dim=8):
     x = layers.BatchNormalization()(x)
     x = layers.LeakyReLU()(x)
     x = layers.AveragePooling1D(2, strides=2)(x)
-
 
     x = layers.Flatten()(x)
 
@@ -146,7 +139,7 @@ def generator_loss(fake_output):
 def load_furious_traces(n, timepoint, window):
     traces = None
 
-    for i in range(1):
+    for i in range(19):
         file = TRACES_FOLDER_FURIOUS + ('random_keys_traces_{}'.format(i)) + '.npy'
         print('Loading {}'.format(file))
         traces_full = np.load(file, allow_pickle=True)[:, timepoint - window // 2: timepoint + window // 2]
@@ -198,7 +191,7 @@ def reshaped_gan(dataset):
     return reshaped_traces
 
 
-def load_dataset_gan(n_traces=200000, variable=None, training=True, window=1000):
+def load_dataset_gan(n_traces=200000, variable=None, window=1000):
     values = np.load(REALVALUES_FOLDER_FURIOUS + 's' + '.npy')[VARIABLE_LIST['s1'].index(variable), :]
     timepoint = np.load(TIMEPOINTS_FOLDER_FURIOUS + 's' + '.npy')[VARIABLE_LIST['s1'].index(variable)]
     traces = load_furious_traces(n_traces, timepoint, window)
--- trace_generation.py
+++ trace_generation.py
@@ -13,7 +13,7 @@ if __name__ == "__main__":
     parser.add_argument('-n', action="store", dest="N", help='Number of real traces compared too',
                         type=int, default=100)    
     
-    args            = parser.parse_args()
+    args = parser.parse_args()
     N = args.N
     SAVE = args.SAVE    
 
@@ -24,15 +24,14 @@ if __name__ == "__main__":
     images = None
     labels = None
     for iterations in range(N//1000):
-        label = np.random.randint(0, high  = n_classes - 1,size = 1000)
+        label = np.random.randint(0, high=n_classes - 1, size=1000)
         noise = tf.random.normal([1000, 100])
         image = np.array(model([noise, label], training=False))
         image = image.reshape(-1,1000)
-        images = image if images is None else np.append(images,image,axis =0)
-        labels = label if labels is None else np.append(labels ,label ,axis =0)
+        images = image if images is None else np.append(images, image, axis=0)
+        labels = label if labels is None else np.append(labels, label, axis=0)
     print(images.shape)
     print(labels.shape)
-    np.save('images.npy',images,allow_pickle= True ) 
-    np.save('labels.npy',labels,allow_pickle= True)
-
+    np.save('images.npy', images, allow_pickle= True )
+    np.save('labels.npy', labels, allow_pickle= True)
 
