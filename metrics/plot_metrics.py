5512c434dc61366b1b9cf3cc1a62116bc4693e3a
Magdalena Solitro <m1solitro@edu.aau.at>
2022-05-05 14:47:21 +0200

    minor fixes

--- metrics/plot_metrics.py
+++ metrics/plot_metrics.py
@@ -10,12 +10,8 @@ from numpy import cov
 from numpy import trace
 from numpy import iscomplexobj
 from numpy.random import random
-#from scipy.linalg import sqrtm
+from scipy.linalg import sqrtm
 import os
-# import tensorflow as tf
-# from tensorflow.keras.models import load_model
-# from gan import make_generator_model,make_discriminator_model
-# from utility import *
 from scalib.metrics import SNR
 
 import pandas as pd
@@ -74,14 +70,14 @@ def string_to_dict():
 
 
 def plot_accuracy_loss():
-    metrics = pd.read_csv('metrics.csv', usecols = ['gen_loss','real_acc','fake_acc','fake_loss','real_loss'])
+    metrics = pd.read_csv('metrics.csv', usecols=['gen_loss', 'real_acc', 'fake_acc', 'fake_loss', 'real_loss'])
 
     d = string_to_dict()
     df = pd.DataFrame(data=d)
     print(df)
 
-    metrics['MA_fake_acc'] = df['fake_acc'].rolling(window = 100).mean()
-    metrics['MA_real_acc'] = df['real_acc'].rolling(window = 100).mean()
+    metrics['MA_fake_acc'] = df['fake_acc'].rolling(window=100).mean()
+    metrics['MA_real_acc'] = df['real_acc'].rolling(window=100).mean()
 
     df = df.astype(float)
 
@@ -115,20 +111,20 @@ if __name__ == "__main__":
 #
 #
 #
-# def calculate_fid(act1, act2):
-# 	# calculate mean and covariance statistics
-# 	mu1, sigma1 = act1.mean(axis=0), cov(act1, rowvar=False)
-# 	mu2, sigma2 = act2.mean(axis=0), cov(act2, rowvar=False)
-# 	# calculate sum squared difference between means
-# 	ssdiff = numpy.sum((mu1 - mu2)**2.0)
-# 	# calculate sqrt of product between cov
-# 	covmean = sqrtm(sigma1.dot(sigma2))
-# 	# check and correct imaginary numbers from sqrt
-# 	if iscomplexobj(covmean):
-# 		covmean = covmean.real
-# 	# calculate score
-# 	fid = ssdiff + trace(sigma1 + sigma2 - 2.0 * covmean)
-# 	return fid
+def calculate_fid(act1, act2):
+	# calculate mean and covariance statistics
+	mu1, sigma1 = act1.mean(axis=0), cov(act1, rowvar=False)
+	mu2, sigma2 = act2.mean(axis=0), cov(act2, rowvar=False)
+	# calculate sum squared difference between means
+	ssdiff = numpy.sum((mu1 - mu2)**2.0)
+	# calculate sqrt of product between cov
+	covmean = sqrtm(sigma1.dot(sigma2))
+	# check and correct imaginary numbers from sqrt
+	if iscomplexobj(covmean):
+		covmean = covmean.real
+	# calculate score
+	fid = ssdiff + trace(sigma1 + sigma2 - 2.0 * covmean)
+	return fid
 #
 #
 # def assess_gan(distorsion):
