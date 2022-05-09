0a499bd8dd369f320b713eae9f63d1bf32b3ef9b
Magdalena Solitro <m1solitro@edu.aau.at>
2022-05-09 12:25:07 +0200

    Fixes on plot_metrics.py

--- metrics/plot_metrics.py
+++ metrics/plot_metrics.py
@@ -9,7 +9,6 @@ import numpy as np
 from numpy import cov
 from numpy import trace
 from numpy import iscomplexobj
-from numpy.random import random
 
 from scipy.linalg import sqrtm
 from scalib.metrics import SNR
@@ -26,7 +25,7 @@ fake_acc = []
 fake_loss = []
 real_loss = []
 
-REAL_TRS_PATH = '/home/solitroma/Desktop/small project/dataset_joey/tracedata/random_keys_traces_1.npy'
+REAL_TRS_PATH = '/home/solitroma/Desktop/small project/dataset_joey/tracedata/random_keys_traces_5.npy'
 REAL_LB_PATH = '/home/solitroma/Desktop/small project/dataset_joey/realvalues/s.npy'
 
 FAKE_TRS_PATH = '/home/solitroma/Desktop/small project/data-augmentation-cgan/metrics/images.npy'
@@ -99,20 +98,18 @@ def plot_accuracy_loss():
 
     df = df.astype(float)
 
-    # plot1 = plt.figure(1)
-    # ax = plt.gca()
-    # metrics.plot(y='MA_fake_acc', ax=ax)
-    # metrics.plot(y='MA_real_acc', ax=ax)
-    #
-    # plot2 = plt.figure(2)
-    # ax = plt.gca()
-    # df.plot(y='gen_loss', ax=ax)
-    # df.plot(y='fake_loss', ax=ax)
-    # df.plot(y='real_loss', ax=ax)
-    #
-    # plt.show()
+    plot1 = plt.figure(1)
+    ax = plt.gca()
+    metrics.plot(y='MA_fake_acc', ax=ax)
+    metrics.plot(y='MA_real_acc', ax=ax)
 
-    #gl.plot(ax = ax)
+    plot2 = plt.figure(2)
+    ax = plt.gca()
+    df.plot(y='gen_loss', ax=ax)
+    df.plot(y='fake_loss', ax=ax)
+    df.plot(y='real_loss', ax=ax)
+
+    plt.show()
 
 
 def calculate_fid(act1, act2):
@@ -155,16 +152,15 @@ def calculate_snr(fake_trs_path, fake_labels_path, real_trs_path, real_labels_pa
     # type conversion required for snr computation
     fake_traces = fake_traces.astype(np.int16)
 
-    # load the labels
-    fake_labels = np.load(fake_labels_path)
-    # type conversion required for snr computation
-    fake_labels = fake_labels.astype(np.uint16)
+    fake_labels = np.load(fake_labels_path)         # load the labels
+    fake_labels = fake_labels.astype(np.uint16)     # type conversion required for snr computation
     fake_labels = np.reshape(fake_labels, (10000, 1))
 
     # compute snr values
     snr_fake = SNR(255, 1000)
     snr_fake.fit_u(fake_traces, fake_labels)
     snr_val_fake = snr_fake.get_snr()
+    snr_val_fake_mean = np.mean(snr_val_fake, axis=0)
 
     # load real traces
     real_traces = np.load(real_trs_path)
@@ -173,8 +169,7 @@ def calculate_snr(fake_trs_path, fake_labels_path, real_trs_path, real_labels_pa
     real_traces = real_traces.astype(np.int16)
 
     real_labels = np.load(real_labels_path)
-    # only consider the first intermediate result for the first 10000 traces!
-    real_labels = real_labels[0, :10000]
+    real_labels = real_labels[0, :10000]    # only consider the first intermediate result for the first 10000 traces!
     real_labels = np.reshape(real_labels, (10000, 1))
     real_labels = real_labels.astype(np.uint16)
 
@@ -205,20 +200,36 @@ if __name__ == "__main__":
     # calculate_snr(FAKE_TRS_PATH, FAKE_LB_PATH, REAL_TRS_PATH, REAL_LB_PATH)
 
     # compute FID score
-    start, end = retrieve_window()
+    # start, end = retrieve_window()
+    start, end = 8100, 9100
 
     all_real_traces = np.load(REAL_TRS_PATH)
-    real_trace = all_real_traces[0][start:end]
+    real_traces_mean = np.mean(all_real_traces, axis=0)
+    real_traces_mean = real_traces_mean[start:end]
 
     fake_traces = np.load(FAKE_TRS_PATH)
-    fake_trace = fake_traces[0]
-    fake_trace = normalise_trace(fake_trace)
+    # compute the mean of all traces
+    fake_traces_mean = np.mean(fake_traces, axis=0)
+    fake_traces_mean = normalise_trace(fake_traces_mean)
 
-    fid1 = calculate_fid(fake_trace.reshape(50, 20), real_trace.reshape(50, 20))
-    plt.plot(fake_trace.reshape(50, 20))
+    # Plot real and fake traces
+    # fig, axs = plt.subplots(2, 1)
+    # axs[1].plot(fake_traces_mean)
+    # axs[1].set_title('Fake traces (mean)')
+    # axs[1].set_xlabel('Time points')
+    # axs[1].set_ylabel('Power value')
+    #
+    # axs[0].plot(real_traces_mean)
+    # axs[0].set_title('Real traces (mean)')
+    # axs[0].set_xlabel('Time points')
+    # axs[0].set_ylabel('Power value')
+    #
+    # plt.subplots_adjust(hspace=0.7)
+    #
+    # plt.show()
 
-    plt.show()
-    fid2 = calculate_fid(fake_trace.reshape(50, 20), fake_trace.reshape(50, 20))
+    fid1 = calculate_fid(fake_traces_mean.reshape(50, 20), real_traces_mean.reshape(50, 20))
+    fid2 = calculate_fid(fake_traces_mean.reshape(50, 20), fake_traces_mean.reshape(50, 20))
 
     print(fid1)
     print(fid2)
